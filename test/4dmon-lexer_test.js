1a0dabd7287fc1cf5a10b207b32d403f75afc5b0
Justin <jrussell@ivantagehealth.com>
2013-05-07 10:19:13 -0400

    test(<scope>): Test comment parsing and input cleansing

--- test/4dmon-lexer_test.js
+++ test/4dmon-lexer_test.js
@@ -2,18 +2,65 @@
 
 'use strict';
 
-var expect = require('chai').expect
+var fs = require('fs')
+  , expect = require('chai').expect
   , Lexer = require('../lib/4dmon-lexer.js').Lexer
-  , lexer = new Lexer();
+  , lexer = new Lexer()
+  , fixtures = __dirname + '/fixtures'
+  , fx_comments = fs.readFileSync(fixtures + '/comments.txt').toString()
+  , fx_trailing_whitespace = fs
+      .readFileSync(fixtures + '/trailing_whitespace.txt').toString();
 
-describe('4dmon-lexer, a tokenizer for 4D code', function() {
+describe('4dmon-lexer', function() {
+
+  it.only('should be able to cleanup input code', function() {
+    var cleaned = lexer.clean(fx_trailing_whitespace);
+    expect(cleaned)
+        .to.equal([
+          '// This method has lots of trailing whitespace',
+          'C_BOOLEAN($0)',
+          '$0 := True'
+        ].join('\n\n'));
+  });
   
   it('should recognize identifier tokens', function() {
     // [todo /]
   });
 
+  describe('identifier matching', function() {
+
+    it('should recognize interprocess variables', function() {
+      // [todo /]
+    });
+
+    it('should recognize local variables', function() {
+      // [todo /]
+    });
+
+    it('should recognize built in 4D methods', function() {
+      // [todo /]
+    });
+
+    it('should recognize project methods', function() {
+      // [todo /]
+    });
+
+    it('should should recognize plugin methods', function() {
+      // [todo /]
+    });
+
+    describe('with mixins', function() {
+      it('should recogize methods added at runtime', function() {
+        // [todo /]
+      });
+    });
+
+  });
+
   it('should recognize comment tokens', function() {
-    // [todo /]
+    var tokens = lexer.tokenize(fx_comments);
+    expect(tokens.length).to.equal(1);
+    expect(tokens[0][0]).to.equal('COMMENT');
   });
 
   it('should recognize white space tokens', function() {
