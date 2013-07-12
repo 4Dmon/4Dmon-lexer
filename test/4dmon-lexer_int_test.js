7b202544abe3156d4bb318ab25078f2fe580e3e1
Justin <jus.russell@gmail.com>
2013-07-12 19:22:27 -0400

    fix: Don't allow comparison regexp to reach too far

--- test/4dmon-lexer_int_test.js
+++ test/4dmon-lexer_int_test.js
@@ -13,21 +13,16 @@ var fs = require('fs')
   , fixtures = __dirname + '/fixtures'
   , fx_silly = fs.readFileSync(fixtures + '/silly_method.txt').toString();
 
-describe.skip('4dmon-lexer', function() {
+describe('4dmon-lexer', function() {
   var lexer;
 
   beforeEach(function() {
     lexer = new Lexer();
   });
 
-  it('should ...', function() {
-    var toks = lexer.tokenize('C_BOOLEAN($0;$1;$truthy)');
-    console.log(toks);
-  });
-
-  it('should be able to churn through a big ol method', function() {
+  it.skip('should be able to churn through a big ol method', function() {
     var toks = lexer.tokenize(fx_silly);
-    console.log(toks);
+    // ... 
   });
 
 });
