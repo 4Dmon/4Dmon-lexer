118efb2f7e219f5bbf7cd220444739990fec9e5b
Justin <jrussell@ivantagehealth.com>
2013-07-12 07:52:54 -0400

    test: Add better operator tests

--- test/4dmon-lexer_test.js
+++ test/4dmon-lexer_test.js
@@ -123,43 +123,22 @@ describe('4dmon-lexer', function() {
       expect(lexer.tokens[0][0]).to.equal('STRING');
     });
 
-    it('should recognize operator tokens', function() {
-      var logicOperators = ['&', '|']
-        , arithmaticOperators = ['+', '-', '*', '/']
-        , comparisonOperators = ['<=', '>=', '<', '>', '=', '#']
-        , i, token;
+    describe('operator tokens recognition', function() {
+      var ops = [
+        '&', '|', // Logic Operators
+        '+', '-', '*', '/', // Arithmatic Operators
+        '<=', '>=', '<', '>', '=', '#' // Comparison Operators
+      ];
 
-      lexer.chunk =
-          logicOperators.join('') +
-          arithmaticOperators.join('') +
-          comparisonOperators.join('');
-
-      for(i = 0; i < logicOperators.length; i++) {
-        expect(lexer.operatorToken())
-            .to.equal(logicOperators[i].length);
-        token = lexer.tokens.pop();
-        expect(token[0]).to.equal('OPERATOR');
-        expect(token[1]).to.equal(logicOperators[i]);
-        lexer.chunk = lexer.chunk.substring(token[1].length);
-      }
-
-      for(i = 0; i < arithmaticOperators.length; i++) {
-        expect(lexer.operatorToken())
-            .to.equal(arithmaticOperators[i].length);
-        token = lexer.tokens.pop();
-        expect(token[0]).to.equal('OPERATOR');
-        expect(token[1]).to.equal(arithmaticOperators[i]);
-        lexer.chunk = lexer.chunk.substring(token[1].length);
-      }
-
-      for(i = 0; i < comparisonOperators.length; i++) {
-        expect(lexer.operatorToken())
-            .to.equal(comparisonOperators[i].length);
-        token = lexer.tokens.pop();
-        expect(token[0]).to.equal('OPERATOR');
-        expect(token[1]).to.equal(comparisonOperators[i]);
-        lexer.chunk = lexer.chunk.substring(token[1].length);
-      }
+      ops.forEach(function(op) {
+        it('should recognize operator ' + op, function() {
+          lexer.chunk = op;
+          expect(lexer.operatorToken()).to.equal(op.length);
+          var tok = lexer.tokens.pop();
+          expect(tok[0]).to.equal('OPERATOR');
+          expect(tok[1]).to.equal(op);
+        });
+      });
 
     });
 
